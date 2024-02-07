15ca07b49065ad7690ae9d7d977f81aa8a891f5a
Hans9806 <gchan9806@gmail.com>
2024-02-07 09:38:38 +0900

    commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+// 주석 : 프로그램과 상관없이 코드에 붙인 설명
+// 한줄 주석 :
+public class HelloWorld { // 클래스 선언부
+    public static void main(String[] args){ // 메소드 선언부
+        // main 메소드는 진입점 entry point
+        System.out.println("Hello, World!");
+    }
+}
+/*
+여러줄 주석
+
+*/
