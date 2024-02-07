ddea2095178f8a34787aab2bd87298782b7abff8
Hans9806 <gchan9806@gmail.com>
2024-02-07 11:43:40 +0900

    연습문제

---
+++ src/variable/Ex1.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        // 연습문제 1 : 5 와 3을 다른 숫자로 한번에 변경할 수 있게 변수로 선언해주세요
+        System.out.println(5 + 3);
+        System.out.println(5 - 3);
+        System.out.println(5 * 3);
+
+        int num1 = 4;
+        int num2 = 7;
+        System.out.println(num1 + num2);
+        System.out.println(num1 + num2);
+        System.out.println(num1 + num2);
+    }
+}
